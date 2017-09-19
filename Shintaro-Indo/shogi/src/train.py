9370b875df9ab63ee40652015a22331490673bd0
Shintaro-Indo <shintaro.indo@gmail.com>
2017-09-20 01:37:10 +0900

    import serializers

--- Shintaro-Indo/shogi/src/train.py
+++ Shintaro-Indo/shogi/src/train.py
@@ -18,6 +18,7 @@ import chainer
 from chainer import cuda
 from chainer import optimizers
 import chainer.links as L
+from chainer import serializers
 
 
 # モデルの候補
@@ -28,8 +29,8 @@ models = {
 }
 
 
-# GPUが使える場合はGPU対応に
-gpu = False
+# GPUを使える場合はGPU対応に
+gpu = False # True：GPU使用可能，False：GPU使用不可
 try:
     gpu_device = 0
     cuda.get_device(gpu_device).use()
@@ -38,16 +39,18 @@ try:
 except:
     xp = np
 
+
 # 前処理
 def preprocessing():
+
     # データの読み込み
     koma = load_data()
     x = koma.data
-    x = x.reshape(x.shape[0], 3, 80, 64) # (データ数，チャネル数(色数)，縦，横)の形式にする．
+    x = x.reshape(x.shape[0], 3, 80, 64) # (データ数，チャネル数，縦，横)の形式にする
     y = koma.target
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
-    # Chainerでは実数のタイプはfloat32, 整数のタイプはint32に固定しておく必要がある．
+    # Chainerでは数値データを32bit型にする必要がある．
     x_train = x_train.astype(xp.float32) # (40681, 80, 64, 3)
     y_train = y_train.astype(xp.int32) # (40681,)
     x_test = x_test.astype(xp.float32)
@@ -61,10 +64,9 @@ def preprocessing():
 
 # 訓練データに対する正答率，誤差を表示する関数
 def train(model, optimizer, x_data, y_data, batchsize=10):
+
+    x_data, y_data = shuffle(x_data, y_data) # 学習する順番をランダムに入れ替える
     N = x_data.shape[0] # データ数
-    x_data, y_data = shuffle(x_data, y_data) # 学習する順番をランダムに入れ替え
-
-
     sum_accuracy = 0 # 累計正答率
     sum_loss = 0 # 累計誤差
     start = time.time() # 開始時刻
@@ -74,8 +76,7 @@ def train(model, optimizer, x_data, y_data, batchsize=10):
         x = chainer.Variable(xp.asarray(x_data[i: i+batchsize]))
         t = chainer.Variable(xp.asarray(y_data[i: i+batchsize]))
 
-        # パラメータの更新(学習)
-        optimizer.update(model, x, t)
+        optimizer.update(model, x, t　# パラメータの更新
 
         sum_loss += float(model.loss.data) * len(t.data) # 累計誤差を更新
         sum_accuracy += float(model.accuracy.data) * len(t.data) # 累計正答率を更新
@@ -83,23 +84,22 @@ def train(model, optimizer, x_data, y_data, batchsize=10):
     end = time.time() # 終了時刻
     elapsed_time = end - start # 所要時間
     throughput = N / elapsed_time # 単位時間当たりの作業量
+
     print("train mean loss={}, accuracy={}, throughput={} image/sec".format(sum_loss / N, sum_accuracy / N, throughput))
 
 
 # テストデータに対する正答率，誤差を表示する関数
 def test(model, x_data, y_data, batchsize=10):
-    N = x_data.shape[0]
-    x_data, y_data = shuffle(x_data, y_data)
 
-    sum_accuracy = 0
-    sum_loss = 0
+    x_data, y_data = shuffle(x_data, y_data) # 学習する順番をランダムに入れ替える
+    N = x_data.shape[0] # データ数
+    sum_accuracy = 0 # 累計正答率
+    sum_loss = 0 # 累計誤差
 
     for i in tqdm(range(0, N, batchsize)):
-        # 評価の時はvolatile *volatileをTrueにするとBackpropergationできない
         x = chainer.Variable(xp.asarray(x_data[i: i+batchsize]))
         t = chainer.Variable(xp.asarray(y_data[i: i+batchsize]))
 
-        # 評価
         loss = model(x, t)
         sum_loss += float(loss.data) * len(t.data)
         sum_accuracy += float(model.accuracy.data) * len(t.data)
@@ -108,41 +108,39 @@ def test(model, x_data, y_data, batchsize=10):
 
 
 if __name__ == "__main__":
-    model_name = sys.argv[1]
-    if len(sys.argv) == 2 and model_name in models.keys(): # コマンドライン引数が条件を満たしているとき
-        # データの準備
+
+    if len(sys.argv) == 2 and sys.argv[1]  in models.keys(): # コマンドライン引数が条件を満たしているとき
+
+        # Step1. データの準備
         x_train, y_train, x_test, y_test = preprocessing()
 
-        # モデルと最適化アルゴリズムの設定
+        # Step2. モデルと最適化アルゴリズムの設定
+        model_name = sys.argv[1] # コマンドライン引数からモデル名を取得
         model = L.Classifier(models[model_name]) # モデルの生成
 
-        # 学習済みモデルが存在する場合は利用する
+        ## 学習済みモデルが存在する場合は利用する
         try:
             serializers.load_npz("{}.npz".format(model_name), model)
         except:
             pass
 
-        # GPUが使える場合はGPU対応に，
+        ## GPUが使える場合はGPU対応に，
         if gpu:
             model = model.to_gpu(gpu_device)
 
         optimizer = optimizers.Adam() # 最適化アルゴリズムの選択
         optimizer.setup(model) # アルゴリズムにモデルをフィット
 
-        # 学習
+        # Step3. 学習
         n_epoch = 10 # 学習回数(学習データを何周するか)
         for epoch in range(1, n_epoch + 1):
             print("\nepoch", epoch)
-
-            # 訓練
             train(model, optimizer, x_train, y_train, batchsize=100)
-
-            # 評価
             test(model, x_test, y_test, batchsize=100)
 
-        # モデルの保存
+        # Step4. モデルの保存
         model.to_cpu() # CPUで計算できるようにしておく
         serializers.save_npz("{}.npz".format(model_name), model) # npz形式で書き出し
 
-    else: # 例外処理
+    else: # コマンドライン引数が条件を満たさないとき
         print("please specify the model (mlp, cnn or resnet) like $ python train.py cnn")
